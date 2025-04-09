5a3411e0d2fe3255bc0fd22b4fe69de1dd45f0dc
OliEfr <ollihaus@t-online.de>
2025-04-09 13:36:02 +0000

    add deps for refinement

--- demo_refine.sh
+++ demo_refine.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:Align3R
-CUDA_VISIBLE_DEVICES=0 python tool/demo_refine.py --input input_dir/scene_name --output_dir demo_dir --seq_name scene_name --interval=50
+CUDA_VISIBLE_DEVICES=0 python tool/demo_refine.py --input assets/maila --output_dir output/maila_refine --seq_name maila --interval=100
