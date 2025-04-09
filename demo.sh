725f4176f5db322c108e9cd8a2dc643f9ad3c45d
OliEfr <ollihaus@t-online.de>
2025-04-09 13:03:30 +0000

    small updates

--- demo.sh
+++ demo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:Align3R
-CUDA_VISIBLE_DEVICES=0 python tool/demo.py --input assets/maila --output_dir output --seq_name maila --interval=50
+CUDA_VISIBLE_DEVICES=0 python tool/demo.py --input assets/maila --output_dir output --seq_name maila --interval=100
